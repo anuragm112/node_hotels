7e9658e01c3e8ce982756b6550361153f022d9ec
Anurag Miglani <125270589+anuragm112@users.noreply.github.com>
2024-03-27 20:52:23 +0530

    Second Version

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 module.exports=async()=>{
     const mongodbUrl=process.env.MONGODB_URL;
      try{
+        //mongodb globally database
          const connect=await mongoose.connect(mongodbUrl,{useNewUrlParser: true, useUnifiedTopology: true});
          console.log(`MongoDB connected: ${connect.connection.host}`);
      }catch(e){
